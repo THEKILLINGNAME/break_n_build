47031ad4b3809912413bb52420d2979ff341ae1f
THEKILLINGNAME <amsinooss@gmail.com>
2022-04-13 17:01:02 +0600

    Define colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,6 +8,23 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'alabaster': '#F9F9F9',
+        'seashell': '#F1F1F1',
+        'mercury': '#E8E8E8',
+        'alto': '#DEDEDE',
+        'dusty-gray': '#9C9C9C',
+        'mine-shaft': '#1F1F1F',
+        'light-brown': '#FBB286',
+        'brown': '#DB986F',
+        'dark-brown': '#9E4E1E',
+        'apricot-peach': '#FBC9AA',
+        'jaffa': '#E98143',
+        'brandy-rose': '#BD8787',
+        'apple-blossom': '#A54242',
+        'downriver': '#0B194F',
+        'azure': '#3D55B0',
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
