fa5a5ad760b2a9b9e5fbfe617b98e5be76e91b51
THEKILLINGNAME <amsinooss@gmail.com>
2022-04-27 16:28:54 +0600

    Add routes for products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root "home#index"
+
   authenticate :user, lambda { |u| u.admin == true } do
     mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   end
-
   devise_for :users
-  root "home#index"
+
+  resources :products, only: [:index, :show]
 end
